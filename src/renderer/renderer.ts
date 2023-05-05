b9ae8c2860e8f72f98d40fb7a0965af2f80e4932
Danny Li <dannyli0109@gmail.com>
2023-05-06 07:38:37 +0800

    comments

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -4,11 +4,21 @@ import { Shader } from "./shader";
 import { Texture } from "./texture";
 import { Vector3 } from "./vector3";
 
+// Create a new interface to define renderer methods
 export interface Renderer {
+    // Define a method to render a Mesh with a Shader
     rederMesh(mesh: Mesh, shader: Shader): void;
+
+    // Define a method to create a Texture
     createTexture(path: string): Texture;
+
+    // Define a method to bind a Texture
     bindTexture(textrue: Texture): void;
 
+    // Define a method to set the Light position
     setLightPosition(position: Vector3): void;
+
+    // Define a method to set the Light color
     setLightColor(color: Color): void;
+    
 }
